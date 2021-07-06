575e2e790ee86a4c30a3833a96815dbf22c464c8
hugoclaude <hugocayeros@gmail.com>
2021-07-06 10:33:29 -0700

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const express = require('express');
+import express from 'express';
+import mysql from "mysql";
 
 const app = express();
+
+const db = mysql.createConnection({
+    // change to IP address once added to heroku
+    host: localhost,
+    user: 'root',
+    password: '',
+    database: 'nodejs-login',
+})
+
+db.connect((error) => {
+     if(error) {
+         console.log(error)
+     } else {
+         console.log("MYSQL Connected...")
+     }
+})
+
+// http://localhost/phpmyadmin/ 
 
 app.get("/", (req, res) => {
     res.send("<h1>Home page</h1>")
